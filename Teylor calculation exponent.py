b04e8c57c6a8abf468c9da419ac0d333414a2e81
Alexander Kolbeko <sasha-kolbeko@mail.ru>
2021-09-15 10:30:41 +0500

    minimuze Scipy function

---
+++ Teylor calculation exponent.py
@@ -0,0 +1,6 @@
+import math
+from sys import set_asyncgen_hooks
+
+Sasha = 1
+set_asyncgen_hooks( )
+print("")
---
+++ minimizationFunction/minimizeFunction.py
@@ -0,0 +1,13 @@
+from scipy import optimize as opt
+import math
+
+
+def function_for_minimize(x):
+    return math.sin(x/5) * math.exp(x/10) + 5 * math.exp(-x/2)
+
+
+minVal = opt.minimize(function_for_minimize, 2)
+print(f'результат работы функции minimize для начальной точки = 2: \n', minVal, f"\n {'-'*10}")
+
+minVal = opt.minimize(function_for_minimize, 30)
+print(f'результат работы функции minimize для начальной точки = 30: \n', minVal, f"\n {'-'*10}")
